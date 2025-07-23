a2a2991bcda450b8b18d3050a148ef333adf35c6
Brenten Lovato <brentengust@gmail.com>
2025-07-23 11:03:10 -0700

    configuring

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
+        pathPrefix: "/portfolio",
         // googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
         //     anonymize: true, // Default true
